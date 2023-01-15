dd60d9061b771256c5c44edb7dbe951685a58350
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2023-01-16 08:30:30 +0900

    default config generator

--- main/machbase-neo/main.go
+++ main/machbase-neo/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/machbase/booter"
 	_ "github.com/machbase/cemlib/banner"
 	"github.com/machbase/neo-server/mods"
@@ -8,6 +12,10 @@ import (
 )
 
 func main() {
+	if len(os.Args) > 1 && strings.ToLower(os.Args[1]) == "gen-config" {
+		fmt.Println(string(server.DefaultFallbackConfig))
+		return
+	}
 	booter.SetFallbackConfig(server.DefaultFallbackConfig)
 	booter.SetFallbackPname(server.DefaultFallbackPname)
 	booter.SetVersionString(mods.VersionString() + " " + mods.EngineInfoString())
