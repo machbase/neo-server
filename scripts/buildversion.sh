945ed60d2bdcda44699b292012bf406c8ff99518
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2023-02-06 09:25:17 +0900

    devel version

--- scripts/buildversion.sh
+++ scripts/buildversion.sh
@@ -3,12 +3,28 @@ if [ -z $VERSION ]; then
     VERSION="v0.0.0"
 fi
 
-TAGGED=$(git describe --tags --contains HEAD 2> /dev/null) || true
-if [ -z $TAGGED ]; then
+# 'main' branch -> use the latest tag 
+# 'devel-v1.2.3' branch -> get version from branch name
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+VERSION=$(echo $BRANCH | sed -n 's/devel-\(v[0-9.]*\).*/\1/p')
+# VERSION=$(echo $BRANCH | sed -n 's/devel-\(v[0-9]*\([.][0-9]*\([.][0-9]*\)*\)*\)/\1/p')
+
+if [ -z $VERSION ]; then
+    TAGGED=$(git describe --tags --contains HEAD 2> /dev/null) || true
+    if [ -z $TAGGED ]; then
+        RE='v[^0-9]*\([0-9]*\)[.]\([0-9]*\)[.]\([0-9]*\)'
+        MAJOR=`echo $VERSION | sed -e "s#$RE#\1#"`
+        MINOR=`echo $VERSION | sed -e "s#$RE#\2#"`
+        PATCH=`echo $VERSION | sed -e "s#$RE#\3#"`
+        VERSION="v$MAJOR.$MINOR.`expr $PATCH + 1`"
+    fi
+else
     RE='v[^0-9]*\([0-9]*\)[.]\([0-9]*\)[.]\([0-9]*\)'
     MAJOR=`echo $VERSION | sed -e "s#$RE#\1#"`
     MINOR=`echo $VERSION | sed -e "s#$RE#\2#"`
     PATCH=`echo $VERSION | sed -e "s#$RE#\3#"`
-    VERSION="v$MAJOR.$MINOR.`expr $PATCH + 1`"
+    VERSION="v$MAJOR.$MINOR.`expr $PATCH + 0`-devel"
 fi
+
 echo $VERSION
