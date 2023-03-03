d2b2d7999d30393a9ba7ed15ca6374a71a559a6e
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2023-03-03 14:08:08 +0900

    cleaning

--- mods/server/svrconf.go
+++ mods/server/svrconf.go
@@ -9,10 +9,6 @@ var DefaultFallbackConfig []byte
 
 var DefaultFallbackPname string = "neo"
 
-// import hcl "github.com/hashicorp/hcl/v2"
 func (s *svr) GetConfig() string {
-	//hcl.Mar https://github.com/alecthomas/hcl
-	// hcl.Marshal(c)
-	// return s.conf.String()
 	return string(DefaultFallbackConfig)
 }
