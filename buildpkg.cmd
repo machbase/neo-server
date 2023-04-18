d094b5896e4a554452858060dbeb14fe170241be
Kwon, Yeong Eon <eirny@carrotins.com>
2023-04-18 17:15:25 +0900

    fix windows package

--- buildpkg.cmd
+++ buildpkg.cmd
@@ -1,5 +1,5 @@
-@build.cmd
-
 @if not exist .\packages md packages
+
+@SET /p VERSION=<.\tmp\version.txt
 
 @powershell Compress-Archive -Force -DestinationPath ".\packages\machbase-neo-fog-%VERSION%-windows-amd64.zip" -Path ".\tmp\*neo*.exe"
