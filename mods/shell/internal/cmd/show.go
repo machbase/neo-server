3858b46e9c22a545a1f54a049b98a62333efc891
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2023-04-19 11:00:48 +0900

    show indexgap

--- mods/shell/internal/cmd/show.go
+++ mods/shell/internal/cmd/show.go
@@ -6,8 +6,10 @@ import (
 	"time"
 
 	"github.com/chzyer/readline"
+	"github.com/machbase/neo-server/mods/codec"
 	"github.com/machbase/neo-server/mods/do"
 	"github.com/machbase/neo-server/mods/shell/internal/client"
+	"github.com/machbase/neo-server/mods/stream"
 	"github.com/machbase/neo-server/mods/util"
 	spi "github.com/machbase/neo-spi"
 )
@@ -25,6 +27,7 @@ func init() {
 const helpShow = `  show [options] <command>
   commands:
     info             show server info
+    indexgap         show index gap
     tables           list tables
       -a,--all       includes all hidden tables
     meta-tables      show meta tables
@@ -32,8 +35,9 @@ const helpShow = `  show [options] <command>
 `
 
 type ShowCmd struct {
-	Info   struct{} `cmd:""`
-	Tables struct {
+	Info     struct{} `cmd:""`
+	IndexGap struct{} `cmd:"" name:"indexgap"`
+	Tables   struct {
 		ShowAll bool `name:"all" short:"a"`
 	} `cmd:""`
 	MetaTables    struct{} `cmd:""`
@@ -44,6 +48,7 @@ type ShowCmd struct {
 func pcShow() readline.PrefixCompleterInterface {
 	return readline.PcItem("show",
 		readline.PcItem("info"),
+		readline.PcItem("indexgap"),
 		readline.PcItem("tables"),
 		readline.PcItem("meta-tables"),
 		readline.PcItem("virtual-tables"),
@@ -70,6 +75,8 @@ func doShow(ctx *client.ActionContext) {
 	switch parserCtx.Command() {
 	case "info":
 		doShowInfo(ctx)
+	case "indexgap":
+		doShowIndexGap(ctx)
 	case "tables":
 		doShowTables(ctx, cmd.Tables.ShowAll)
 	case "meta-tables":
@@ -79,6 +86,57 @@ func doShow(ctx *client.ActionContext) {
 	default:
 		ctx.Println(helpShow)
 		return
+	}
+}
+
+func doShowIndexGap(ctx *client.ActionContext) {
+	sqlText := `select 
+		b.name as TABLE_NAME, 
+		c.name as INDEX_NAME, 
+		a.table_end_rid - a.end_rid as GAP
+	from
+		v$storage_dc_table_indexes a,
+		m$sys_tables b, m$sys_indexes c
+	where
+		a.id = c.id 
+	and c.table_id = b.id 
+	order by 3 desc`
+
+	var output spi.OutputStream
+	output, err := stream.NewOutputStream("-")
+	if err != nil {
+		ctx.Println("ERR", err.Error())
+	}
+	defer output.Close()
+
+	encoder := codec.NewEncoderBuilder(codec.BOX).
+		SetOutputStream(output).
+		SetRownum(true).
+		SetHeading(true).
+		SetBoxStyle(ctx.Pref().BoxStyle().Value()).
+		Build()
+
+	queryCtx := &do.QueryContext{
+		DB: ctx.DB,
+		OnFetchStart: func(cols spi.Columns) {
+			encoder.Open(cols)
+		},
+		OnFetch: func(nrow int64, values []any) bool {
+			err := encoder.AddRow(values)
+			if err != nil {
+				ctx.Println("ERR", err.Error())
+			}
+			return true
+		},
+		OnFetchEnd: func() {
+			encoder.Close()
+		},
+	}
+	msg, err := do.Query(queryCtx, sqlText)
+	if err != nil {
+		ctx.Println("ERR", err.Error())
+	} else {
+		ctx.Println(msg)
 	}
 }
 
