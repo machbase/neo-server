33909f23fc5c9f2126d81b2dedff25fcb7154450
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2023-05-11 17:20:22 +0900

    add status lights

--- main/neow/res/res.go
+++ main/neow/res/res.go
@@ -7,3 +7,12 @@ var Logo []byte
 
 //go:embed D2Coding-Ver1.3.2-20180524.ttf
 var D2Coding []byte
+
+//go:embed sig_green.png
+var CircleGreen []byte
+
+//go:embed sig_yellow.png
+var CircleYellow []byte
+
+//go:embed sig_red.png
+var CircleRed []byte
