70ac39f967751f1d81557627a34d4df7df263793
Kwon, Yeong Eon <eirny@carrotins.com>
2023-05-17 10:49:48 +0900

    shutdown on windows

--- mods/server/mgmt_shutdown.go
+++ mods/server/mgmt_shutdown.go
@@ -15,7 +15,7 @@ import (
 func (s *svr) Shutdown(ctx context.Context, req *mgmt.ShutdownRequest) (*mgmt.ShutdownResponse, error) {
 	tick := time.Now()
 	rsp := &mgmt.ShutdownResponse{}
-	if runtime.GOOS == "windows" {
+	if runtime.GOOS != "windows" {
 		p, _ := peer.FromContext(ctx)
 		s.log.Infof("shutdown request from %v", p.Addr)
 		if !strings.HasPrefix(p.Addr.String(), "127.0.0.1:") && !strings.HasPrefix(p.Addr.String(), "0:0:0:0:0:0:0:1") {
