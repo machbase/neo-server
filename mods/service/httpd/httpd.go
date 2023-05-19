349580ee41b14a04cec4a8b846d69f0a6b255116
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2023-05-19 11:08:29 +0900

    chart api with GET

--- mods/service/httpd/httpd.go
+++ mods/service/httpd/httpd.go
@@ -226,6 +226,7 @@ func (svr *httpd) Router() *gin.Engine {
 			group.GET("/api/check", svr.handleCheck)
 			group.POST("/api/relogin", svr.handleReLogin)
 			group.POST("/api/logout", svr.handleLogout)
+			group.GET("/api/chart", svr.handleChart)
 			group.POST("/api/chart", svr.handleChart)
 			group.GET("/api/tables", svr.handleTables)
 			group.GET("/api/tables/:table/tags", svr.handleTags)
