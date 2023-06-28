e2ccfc5557376fe923a9b915d17e7a4ce541f212
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2023-06-28 14:10:15 +0900

    remove md asset control

--- mods/service/httpd/h_md.go
+++ mods/service/httpd/h_md.go
@@ -2,7 +2,6 @@ package httpd
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"net/http"
 
@@ -11,26 +10,6 @@ import (
 	"github.com/yuin/goldmark/extension"
 	"github.com/yuin/goldmark/renderer/html"
 )
-
-var mdHtmlHeader = `
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="/web/assets/github-markdown.css">
-   <link rel="stylesheet" href="/web/assets/github-markdown-%s.css">
-   <title>%s</title>
-</head>
-<body>
-<br>
-<article class="markdown-body">`
-
-var mdHtmlFooter = []byte(`
-</article>
-<br>
-</body>
-</html>`)
 
 // POST "/md"
 func (svr *httpd) handleMarkdown(ctx *gin.Context) {
@@ -54,18 +33,6 @@ func (svr *httpd) handleMarkdown(ctx *gin.Context) {
 		ctx.String(http.StatusInternalServerError, err.Error())
 		return
 	}
-	addHeader := strBool(ctx.Query("header"), true)
-	title := strString(ctx.Query("title"), "")
-	lightOrDark := "light"
-	if strBool(ctx.Query("dark"), false) {
-		lightOrDark = "dark"
-	}
 	ctx.Writer.Header().Set("Content-Type", "text/html")
-	if addHeader {
-		ctx.Writer.Write([]byte(fmt.Sprintf(mdHtmlHeader, lightOrDark, title)))
-	}
 	ctx.Writer.Write(dst.Bytes())
-	if addHeader {
-		ctx.Writer.Write(mdHtmlFooter)
-	}
 }
