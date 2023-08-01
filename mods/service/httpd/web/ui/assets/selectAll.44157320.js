e009f50c7d711e9db0725c491d1871607549011a
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2023-08-01 09:35:49 +0900

    fix taz ui bug

--- mods/service/httpd/web/ui/assets/selectAll.44157320.js
+++
@@ -1,1 +0,0 @@
-import{a1 as t,a2 as n,a3 as o}from"./index.e8a8b836.js";function r(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{r as s};
