2095cfb54af808caef6d0eb16becd95a8f4d7d3e
lshdainty <alex.lee@machbase.com>
2023-08-23 08:57:08 +0900

    line protocol test case commit

--- mods/service/httpd/h_lineprotocol_test.go
+++ mods/service/httpd/h_lineprotocol_test.go
@@ -2,6 +2,7 @@ package httpd
 
 import (
 	"bytes"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,7 +13,15 @@ import (
 )
 
 const (
-	H_LINE_DESC_QUERYROW_SQL string = `SELECT
+	LINEPROTOCOLDATA = `cpu,cpu=cpu-total,host=desktop usage_irq=0,usage_softirq=0.004171359446037821,usage_guest=0,usage_user=0.3253660367906774,usage_system=0.0792558294748905,usage_idle=99.59120677410203,usage_guest_nice=0,usage_nice=0,usage_iowait=0,usage_steal=0 1670975120000000000
+mem,host=desktop committed_as=8780218368i,dirty=327680i,huge_pages_free=0i,shared=67067904i,sreclaimable=414224384i,total=67377881088i,buffered=810778624i,vmalloc_total=35184372087808i,active=3356581888i,available_percent=95.04513097460023,free=56726638592i,slab=617472000i,available=64039395328i,vmalloc_used=54685696i,cached=7298387968i,inactive=6323064832i,low_total=0i,page_tables=32129024i,high_free=0i,commit_limit=35836420096i,high_total=0i,swap_total=2147479552i,write_back_tmp=0i,write_back=0i,used=2542075904i,swap_cached=0i,vmalloc_chunk=0i,mapped=652132352i,huge_page_size=2097152i,huge_pages_total=0i,low_free=0i,sunreclaim=203247616i,swap_free=2147479552i,used_percent=3.7728641253646424 1670975120000000000
+disk,device=nvme0n1p3,fstype=ext4,host=desktop,mode=rw,path=/ total=1967315451904i,free=1823398948864i,used=43906785280i,used_percent=2.3513442109214915,inodes_total=122068992i,inodes_free=121125115i,inodes_used=943877i 1670975120000000000
+system,host=desktop n_users=2i,load1=0.08,load5=0.1,load15=0.09,n_cpus=24i 1670975120000000000
+system,host=desktop uptime=513536i 1670975120000000000
+system,host=desktop uptime_format="5 days, 22:38" 1670975120000000000
+processes,host=desktop zombies=0i,unknown=0i,dead=0i,paging=0i,total_threads=1084i,blocked=0i,stopped=0i,running=0i,sleeping=282i,total=426i,idle=144i 1670975120000000000`
+
+	H_LINE_DESC_QUERYROW_SQL = `SELECT
 			j.ID as TABLE_ID,
 			j.TYPE as TABLE_TYPE,
 			j.FLAG as TABLE_FLAG,
@@ -26,10 +35,11 @@ const (
 		and j.DATABASE_ID = ?
 		and j.NAME = ?`
 
-	H_LINE_DESC_QUERY_SQL string = "select name, type, length, id from M$SYS_COLUMNS where table_id = ? order by id"
+	H_LINE_DESC_QUERY_SQL = "select name, type, length, id from M$SYS_COLUMNS where table_id = ? order by id"
 )
 
 func TestLineprotocol(t *testing.T) {
+	columnDefaultLen := 4
 	dbMock := &TestClientMock{}
 
 	dbMock.QueryRowFunc = func(sqlText string, params ...any) spi.Row {
@@ -40,7 +50,7 @@ func TestLineprotocol(t *testing.T) {
 			rm.ScanFunc = func(cols ...any) error {
 				if len(params) == 3 {
 					*(cols[0].(*int)) = 0
-					*(cols[1].(*int)) = 6
+					*(cols[1].(*int)) = spi.TagTableType
 					*(cols[2].(*int)) = 0
 					*(cols[3].(*int)) = 0
 				}
@@ -58,7 +68,7 @@ func TestLineprotocol(t *testing.T) {
 
 	dbMock.QueryFunc = func(sqlText string, params ...any) (spi.Rows, error) {
 		rm := &mock.RowsMock{}
-		tCnt := 5
+		tCnt := columnDefaultLen
 		cnt := 0
 
 		switch sqlText {
@@ -79,27 +89,22 @@ func TestLineprotocol(t *testing.T) {
 				switch cnt - 1 {
 				case 0:
 					*(cols[0].(*string)) = "NAME"
-					*(cols[1].(*int)) = 5
+					*(cols[1].(*int)) = spi.VarcharColumnType
 					*(cols[2].(*int)) = 0
 					*(cols[3].(*uint64)) = 0
 				case 1:
 					*(cols[0].(*string)) = "TIME"
-					*(cols[1].(*int)) = 6
+					*(cols[1].(*int)) = spi.DatetimeColumnType
 					*(cols[2].(*int)) = 0
 					*(cols[3].(*uint64)) = 0
 				case 2:
 					*(cols[0].(*string)) = "VALUE"
-					*(cols[1].(*int)) = 20
+					*(cols[1].(*int)) = spi.Float64ColumnType
 					*(cols[2].(*int)) = 0
 					*(cols[3].(*uint64)) = 0
 				case 3:
-					*(cols[0].(*string)) = "SERVER"
-					*(cols[1].(*int)) = 5
-					*(cols[2].(*int)) = 0
-					*(cols[3].(*uint64)) = 0
-				case 4:
-					*(cols[0].(*string)) = "LOCAL"
-					*(cols[1].(*int)) = 5
+					*(cols[0].(*string)) = "HOST"
+					*(cols[1].(*int)) = spi.VarcharColumnType
 					*(cols[2].(*int)) = 0
 					*(cols[3].(*uint64)) = 0
 				}
@@ -113,6 +118,19 @@ func TestLineprotocol(t *testing.T) {
 			return nil
 		}
 		return rm, nil
+	}
+
+	dbMock.ExecFunc = func(sqlText string, params ...any) spi.Result {
+		rm := &mock.ResultMock{}
+
+		if len(params) != columnDefaultLen {
+			t.Fatal(errors.New("column len different"))
+		}
+
+		rm.ErrFunc = func() error {
+			return nil
+		}
+		return rm
 	}
 
 	webService, err := New(dbMock,
@@ -131,20 +149,32 @@ func TestLineprotocol(t *testing.T) {
 	var req *http.Request
 	var expectStatus int
 
+	// success case - line protocol
 	w = httptest.NewRecorder()
-	req, _ = http.NewRequest("POST", "/metrics/write?db=tag", bytes.NewBufferString(""))
+	req, _ = http.NewRequest("POST", "/metrics/write?db=tag", bytes.NewBufferString(LINEPROTOCOLDATA))
 	req.Header.Set("Content-Type", "application/octet-stream")
 	router.ServeHTTP(w, req)
 
-	// test := make(map[string]interface{})
-	// err = json.Unmarshal(w.Body.Bytes(), &test)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-
-	// fmt.Println(test)
-
 	expectStatus = http.StatusNoContent
+	require.Equal(t, expectStatus, w.Code)
+
+	//wrong case - wrong request
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("POST", "/metrics/read?db=tag", bytes.NewBufferString(LINEPROTOCOLDATA))
+	req.Header.Set("Content-Type", "application/octet-stream")
+	router.ServeHTTP(w, req)
+
+	expectStatus = http.StatusNotImplemented
+	require.Equal(t, expectStatus, w.Code)
+
+	//wrong case - gzip wrong request
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("POST", "/metrics/write?db=tag", bytes.NewBufferString(LINEPROTOCOLDATA))
+	req.Header.Set("Content-Type", "application/octet-stream")
+	req.Header.Set("Content-Encoding", "gzip")
+	router.ServeHTTP(w, req)
+
+	expectStatus = http.StatusBadRequest
 	require.Equal(t, expectStatus, w.Code)
 
 	return
