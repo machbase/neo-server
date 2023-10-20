a7ada96c055ae5beec902b6f57a3965f4c709425
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2023-10-20 14:49:59 +0900

    table schema

--- mods/do/tags.go
+++ mods/do/tags.go
@@ -9,7 +9,15 @@ import (
 )
 
 func Tags(ctx context.Context, conn spi.Conn, table string, callback func(string, error) bool) {
-	sqlText := fmt.Sprintf("select * from _%s_META", strings.ToUpper(table))
+	var sqlText string
+	if strings.Contains(table, ".") {
+		idx := strings.LastIndex(table, ".")
+		prefix := table[0:idx]
+		table = table[idx+1:]
+		sqlText = fmt.Sprintf("select * from %s._%s_META", strings.ToUpper(prefix), strings.ToUpper(table))
+	} else {
+		sqlText = fmt.Sprintf("select * from _%s_META", strings.ToUpper(table))
+	}
 	rows, err := conn.Query(ctx, sqlText)
 	if err != nil {
 		callback("", err)
