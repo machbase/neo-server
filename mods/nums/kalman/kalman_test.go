26139b7226c255c83201f406b6cfcf87f72a0864
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2024-03-07 11:59:08 +0900

    test case

--- mods/nums/kalman/kalman_test.go
+++ mods/nums/kalman/kalman_test.go
@@ -1,11 +1,12 @@
 package kalman
 
 import (
-	"fmt"
+	"math"
 	"testing"
 	"time"
 
 	"github.com/machbase/neo-server/mods/nums/kalman/models"
+	"github.com/stretchr/testify/require"
 )
 
 func TestKalman(t *testing.T) {
@@ -19,9 +20,33 @@ func TestKalman(t *testing.T) {
 	})
 	filter := NewKalmanFilter(model)
 
+	var result []float64
 	for _, v := range values {
 		ts = ts.Add(time.Second)
 		filter.Update(ts, model.NewMeasurement(v))
-		fmt.Printf("filtered value: %f\n", model.Value(filter.State()))
+		result = append(result, math.Round(model.Value(filter.State())*100000)/100000)
 	}
+	expect := []float64{1.3, 5.75, 5.375, 4.3875}
+	require.EqualValues(t, expect, result)
+}
+
+func TestKalmanSmoother(t *testing.T) {
+	var ts time.Time
+	values := []float64{1.3, 10.2, 5.0, 3.4}
+
+	model := models.NewSimpleModel(ts, values[0], models.SimpleModelConfig{
+		InitialVariance:     1.0,
+		ProcessVariance:     1.0,
+		ObservationVariance: 2.0,
+	})
+	smoother := NewKalmanSmoother(model)
+
+	var result []float64
+	for _, v := range values {
+		ts = ts.Add(time.Second)
+		states, _ := smoother.Smooth(NewMeasurementAtTime(ts, model.NewMeasurement(v)))
+		result = append(result, math.Round(model.Value(states[0].State)*100000)/100000)
+	}
+	expect := []float64{1.3, 6.64, 3.76667, 2.8}
+	require.EqualValues(t, expect, result)
 }
