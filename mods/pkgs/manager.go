e713e9961cdadb2a4252910062b3a6ae11870b83
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2024-08-09 13:58:10 +0900

    update packages

--- mods/pkgs/manager.go
+++ mods/pkgs/manager.go
@@ -47,24 +47,17 @@ func (pm *PkgManager) AddEnv(key, value string) {
 // if name is empty, it will return all featured packages
 func (pm *PkgManager) Search(name string, possible int) (*pkgs.PackageSearchResult, error) {
 	if name == "" {
-		prj, err := pm.roster.RootMeta()
+		prj, err := pm.roster.FeaturedPackages()
 		if err != nil {
 			return nil, err
 		}
 		ret := &pkgs.PackageSearchResult{}
 		for _, pkg := range prj.Featured {
-			if meta, err := pm.roster.LoadPackageMeta(pkg); err != nil {
-				pm.log.Error("failed to load package meta", pkg, err)
+			cache, err := pm.roster.LoadPackageCache(pkg)
+			if err != nil {
+				pm.log.Error("failed to load package cache", pkg, err)
 			} else {
-				cache, err := pm.roster.LoadPackageCache(pkg, meta, false)
-				if err != nil {
-					pm.log.Error("failed to load package cache", pkg, err)
-				} else {
-					ret.Possibles = append(ret.Possibles, cache)
-				}
-			}
-			if possible > 0 && len(ret.Possibles) >= possible {
-				break
+				ret.Possibles = append(ret.Possibles, cache)
 			}
 		}
 		return ret, nil
@@ -73,31 +66,24 @@ func (pm *PkgManager) Search(name string, possible int) (*pkgs.PackageSearchResu
 	}
 }
 
-func (pm *PkgManager) Install(name string, output io.Writer) (*pkgs.PackageCache, error) {
-	err := pm.roster.Install(name, output, pm.envs)
-	if err != nil {
-		return nil, err
-	}
-	cache, err := pm.roster.ReadCache(name)
-	if err != nil {
-		return nil, err
+func (pm *PkgManager) Install(name string, output io.Writer) (*pkgs.InstallStatus, error) {
+	ret := pm.roster.Install([]string{name}, pm.envs)
+	if len(ret) == 0 {
+		return nil, fmt.Errorf("failed to install %s", name)
 	}
 
-	pm.log.Info("installed", name, cache.InstalledVersion, cache.InstalledPath)
-	return cache, nil
+	pm.log.Info("installed", name, ret[0].Installed.Version, ret[0].Installed.Path)
+	output.Write([]byte(ret[0].Output))
+	return ret[0], nil
 }
 
-func (pm *PkgManager) Uninstall(name string, output io.Writer) (*pkgs.PackageCache, error) {
+func (pm *PkgManager) Uninstall(name string, output io.Writer) error {
 	err := pm.roster.Uninstall(name, output, pm.envs)
 	if err != nil {
-		return nil, err
-	}
-	cache, err := pm.roster.ReadCache(name)
-	if err != nil {
-		return nil, err
+		return err
 	}
 	pm.log.Info("uninstalled", name)
-	return cache, nil
+	return nil
 }
 
 func (pm *PkgManager) HttpAppRouter(r gin.IRouter) {
@@ -105,7 +91,12 @@ func (pm *PkgManager) HttpAppRouter(r gin.IRouter) {
 		name := ctx.Param("name")
 		path := ctx.Param("path")
 		ctx.Request.URL.Path = path
-		fs := http.FileServer(http.Dir(pm.roster.InstallDir(name)))
+		inst, err := pm.roster.InstalledVersion(name)
+		if err != nil {
+			ctx.JSON(404, gin.H{"success": false, "reason": err.Error()})
+			return
+		}
+		fs := http.FileServer(http.Dir(inst.CurrentPath))
 		fs.ServeHTTP(ctx.Writer, ctx.Request)
 	})
 }
@@ -133,7 +124,6 @@ func (pm *PkgManager) HttpPkgRouter(r gin.IRouter) {
 		}
 	})
 	r.GET("/update", pm.doUpdate)
-	r.GET("/upgrade/:name", pm.doUpgrade)
 	r.GET("/install/:name", pm.doInstall)
 	r.GET("/uninstall/:name", pm.doUninstall)
 }
@@ -184,40 +174,6 @@ func (pm *PkgManager) doUpdate(c *gin.Context) {
 	})
 }
 
-func (pm *PkgManager) doUpgrade(c *gin.Context) {
-	ts := time.Now()
-	name := c.Param("name")
-	installed := pm.roster.Upgrade([]string{name}, pm.envs)
-	if len(installed) != 1 {
-		c.JSON(500, gin.H{
-			"success": false,
-			"reason":  fmt.Sprintf("invalid upgrade result %d", len(installed)),
-			"elapse":  fmt.Sprintf("%v", time.Since(ts)),
-		})
-		return
-	}
-	r := installed[0]
-	rsp := gin.H{
-		"success": r.Success,
-		"reason":  "success",
-		"elapse":  fmt.Sprintf("%v", time.Since(ts)),
-	}
-	if !r.Success {
-		if r.Err != nil {
-			rsp["reason"] = r.Err.Error()
-		} else {
-			rsp["reason"] = "unknown error"
-		}
-	}
-	if r.Cache != nil {
-		rsp["data"] = map[string]any{
-			"info": r.Cache,
-			"log":  r.Output,
-		}
-	}
-	c.JSON(200, rsp)
-}
-
 func (pm *PkgManager) doInstall(c *gin.Context) {
 	ts := time.Now()
 	name := c.Param("name")
@@ -248,7 +204,7 @@ func (pm *PkgManager) doUninstall(c *gin.Context) {
 	name := c.Param("name")
 
 	output := &strings.Builder{}
-	cache, err := pm.Uninstall(name, output)
+	err := pm.Uninstall(name, output)
 	if err != nil {
 		c.JSON(500, gin.H{
 			"success": false,
@@ -265,8 +221,7 @@ func (pm *PkgManager) doUninstall(c *gin.Context) {
 		"reason":  "success",
 		"elapse":  fmt.Sprintf("%v", time.Since(ts)),
 		"data": map[string]any{
-			"info": cache,
-			"log":  output.String(),
+			"log": output.String(),
 		},
 	})
 }
