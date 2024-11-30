56e8aad1db083059dbcd2e2aa1f3185ce3f8247c
Kwon, Yeong Eon <happylogin@gmail.com>
2024-11-30 10:50:42 +0900

    moved

--- mods/server/server_test.go
+++ mods/server/server_test.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"strings"
 	"testing"
 	"time"
@@ -138,4 +139,14 @@ func initTestData(db api.Database) {
 		}
 	}
 	conn.Exec(ctx, `EXEC table_flush(example)`)
+}
+
+func Example_representativePort() {
+	fmt.Println(representativePort("tcp://127.0.0.1:1234"))
+	fmt.Println(representativePort("http://192.168.1.100:1234"))
+	fmt.Println(representativePort("unix:///var/run/neo-server.sock"))
+	// Output:
+	//   > Local:   http://127.0.0.1:1234
+	//   > Network: http://192.168.1.100:1234
+	//   > Unix:    /var/run/neo-server.sock
 }
