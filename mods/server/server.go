4c93d75c208285232168992c1353f1e68a0fd00f
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2025-03-04 08:56:11 +0900

    refactor: remove unused functions

--- mods/server/server.go
+++ mods/server/server.go
@@ -1272,34 +1272,6 @@ func mkDirIfNotExistsMode(path string, mode fs.FileMode) error {
 	return nil
 }
 
-//lint:ignore U1000 ignore unused for now
-func makeListener(addr string) (net.Listener, error) {
-	if strings.HasPrefix(addr, "unix://") {
-		pwd, _ := os.Getwd()
-		if strings.HasPrefix(addr, "unix://../") {
-			addr = fmt.Sprintf("unix:///%s", filepath.Join(filepath.Dir(pwd), addr[len("unix://../"):]))
-		} else if strings.HasPrefix(addr, "../") {
-			addr = fmt.Sprintf("unix:///%s", filepath.Join(filepath.Dir(pwd), addr[len("../"):]))
-		} else if strings.HasPrefix(addr, "unix://./") {
-			addr = fmt.Sprintf("unix:///%s", filepath.Join(pwd, addr[len("unix://./"):]))
-		} else if strings.HasPrefix(addr, "./") {
-			addr = fmt.Sprintf("unix:///%s", filepath.Join(pwd, addr[len("./"):]))
-		} else if strings.HasPrefix(addr, "/") {
-			addr = fmt.Sprintf("unix://%s", addr)
-		}
-		path := addr[len("unix://"):]
-		// delete existing .sock file
-		if _, err := os.Stat(path); err == nil {
-			os.Remove(path)
-		}
-		return net.Listen("unix", path)
-	} else if strings.HasPrefix(addr, "tcp://") {
-		return net.Listen("tcp", addr[len("tcp://"):])
-	} else {
-		return nil, fmt.Errorf("unsupported listen scheme %s", addr)
-	}
-}
-
 // ////////////////////////////////
 // implements AuthServer interface
 
