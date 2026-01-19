75c5e3f41b7baf39d2e3306adf0d0a12d8004597
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2026-01-19 14:55:18 +0900

    cli stmt clean

--- api/machcli/machcli.go
+++ api/machcli/machcli.go
@@ -486,15 +486,16 @@ func (pStmt *PreparedStmt) Exec(ctx context.Context, params ...any) api.Result {
 	return ret
 }
 
-func (pStmt *PreparedStmt) Query(ctx context.Context, args ...any) (api.Rows, error) {
-	if err := pStmt.stmt.bindParams(args...); err != nil {
+func (pStmt *PreparedStmt) Query(ctx context.Context, params ...any) (api.Rows, error) {
+	if err := pStmt.stmt.bindParams(params...); err != nil {
 		return nil, err
 	}
 	if err := pStmt.stmt.execute(); err != nil {
 		return nil, err
 	}
 	ret := &Rows{
-		stmt: pStmt.stmt,
+		stmt:       pStmt.stmt,
+		isPrepared: true,
 	}
 	ret.rowsCount = pStmt.stmt.rowCount
 	return ret, nil
@@ -829,6 +830,7 @@ type Stmt struct {
 	reachEOF    bool
 	sqlHead     string
 	rowCount    int64
+	execCount   int64
 }
 
 func (stmt *Stmt) Close() error {
@@ -847,13 +849,17 @@ func (stmt *Stmt) execute() error {
 	if err := mach.CliExecute(stmt.handle); err != nil {
 		return errorWithCause(stmt, err)
 	}
-
+	defer func() {
+		stmt.execCount++
+	}()
 	if rowCount, err := mach.CliRowCount(stmt.handle); err != nil {
 		return errorWithCause(stmt, err)
 	} else {
 		stmt.rowCount = rowCount
 	}
-
+	if stmt.execCount > 0 {
+		return nil
+	}
 	if stmt.sqlHead != "SELECT" {
 		return nil
 	}
@@ -1036,10 +1042,11 @@ func (r *Row) Message() string {
 }
 
 type Rows struct {
-	stmt      *Stmt
-	err       error
-	row       []any
-	rowsCount int64
+	stmt       *Stmt
+	err        error
+	row        []any
+	rowsCount  int64
+	isPrepared bool
 }
 
 var _ api.Rows = (*Rows)(nil)
@@ -1050,7 +1057,11 @@ func (r *Rows) Err() error {
 
 func (r *Rows) Close() error {
 	if r.stmt != nil {
-		return r.stmt.Close()
+		if r.isPrepared {
+			mach.CliExecuteClean(r.stmt.handle)
+		} else {
+			return r.stmt.Close()
+		}
 	}
 	return nil
 }
