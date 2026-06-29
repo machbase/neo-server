bf05a6034be9297148e961e901b03cc4886c96cf
Kwon, Yeong Eon <OutOfBedlam@users.noreply.github.com>
2026-06-29 16:41:44 +0900

    Rename table references in tests and user script from 'table1' to 'neo_scope_t1_ua'

--- mods/server/test/user_script.sql
+++ mods/server/test/user_script.sql
@@ -2,20 +2,20 @@ create user user_a identified by 'password';
 
 connect user_a/password;
 
-create table table1 (id integer);
-insert into table1 values (1);
-insert into table1 values (2);
-insert into table1 values (3);
+create table neo_scope_t1_ua (id integer);
+insert into neo_scope_t1_ua values (1);
+insert into neo_scope_t1_ua values (2);
+insert into neo_scope_t1_ua values (3);
 
-select * from table1;
+select * from neo_scope_t1_ua;
 
 connect sys/manager;
 
-sql --format csv select * from table1;
+sql --format csv select * from neo_scope_t1_ua;
 
-insert into user_a.table1 values (4);
+insert into user_a.neo_scope_t1_ua values (4);
 
-sql --format csv select * from user_a.table1;
+sql --format csv select * from user_a.neo_scope_t1_ua;
 
-drop table user_a.table1;
+drop table user_a.neo_scope_t1_ua;
 drop user user_a;
